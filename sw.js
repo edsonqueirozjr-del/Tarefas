796dd03127f37c84c456b924a4badb25eb761963
edsonqueirozjr-del <edsonqueirozjr@gmail.com>
2026-09-14 20:13:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker simples: deixa o app abrir mesmo sem internet e sempre busca a versão nova quando houver.
-const CACHE='tarefas-v1';
+const CACHE='tarefas-v2';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
