e879d021dd908eb3e38d0bdf24442b821f285e59
edsonqueirozjr-del <edsonqueirozjr@gmail.com>
2026-09-15 13:02:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker: guarda só os arquivos do próprio app; scripts externos (Firebase) passam direto.
-const CACHE='tarefas-v6';
+const CACHE='tarefas-v10';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-192.png','./icon-maskable-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
